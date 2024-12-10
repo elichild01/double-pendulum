5b77d544312091780014bb6ad53ef7e716b9f178
David Thomas Litster <90051576+dt-litster@users.noreply.github.com>
2024-12-10 10:56:57 -0700

    Added one-step error

--- utils/metrics.py
+++ utils/metrics.py
@@ -10,12 +10,7 @@ def time_to_divergence(y, y_hat, lambda_=0, tol=1e-5):
     lambda : regularization parameter
     tol : maximum error
     """
-    theta, theta_hat = y[:, :2], y_hat[:, :2]
-    theta_prime, theta_prime_hat = y[:, 2:], y_hat[:, 2:]
-    
-    divergence = (np.linalg.norm(theta - theta_hat, axis=1) + 
-                  lambda_ * np.linalg.norm(theta_prime - theta_prime_hat, axis=1))
-    return min(np.where(divergence >= tol))
+    return min(np.where(one_step_error(y, y_hat, lambda_) >= tol))
 
 
 def total_divergence_at_time(t_idx, y, y_hat, lambda_=0):
@@ -37,14 +32,27 @@ def total_divergence_at_time(t_idx, y, y_hat, lambda_=0):
 def max_divergence_at_time(t_idx, y, y_hat, lambda_=0):
     """
     Returns max_{t<T} (||theta(t) - theta_hat(t)|| + lambda_ ||theta'(t) - theta_hat'(t)||)
+    Agrees with global error (3.11) when lambda_=1 and t_idx is as large as possible
 
     y : True values of theta (first two columns) and theta' (last two columns)
     y_hat : predicted values of y
     t_idx : The index such that y[t_idx] = y(T)
     lambda : regularization parameter
     """
-    theta, theta_hat = y[:t_idx, :2], y_hat[:t_idx, :2]
-    theta_prime, theta_prime_hat = y[:t_idx, 2:], y_hat[:t_idx, 2:]
+    return max(one_step_error(y, y_hat, lambda_)[:t_idx]) 
     
-    return max(np.linalg.norm(theta - theta_hat, axis=1) + 
-               lambda_ * np.linalg.norm(theta_prime - theta_prime_hat, axis=1)) 
+
+def one_step_error(y, y_hat, lambda_=0):
+    """
+    Returns L(t-1) = ||theta(t) - theta_hat(t)|| + lambda_ ||theta'(t) - theta_hat'(t)||
+    Agrees with (3.13) when lambda_=1.  Note the shift in index.
+
+    y : True values of theta (first two columns) and theta' (last two columns)
+    y_hat : predicted values of y
+    lambda : regularization parameter
+    """
+    theta, theta_hat = y[:, :2], y_hat[:, :2]
+    theta_prime, theta_prime_hat = y[:, 2:], y_hat[:, 2:]
+    
+    return (np.linalg.norm(theta - theta_hat, axis=1) + 
+            lambda_ * np.linalg.norm(theta_prime - theta_prime_hat, axis=1))
